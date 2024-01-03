3beea8ffec249f36581f33bfc246026e74fb1060
Francois DUMAS LATTAQUE <dumaslattaque.francois@gmail.com>
2024-01-03 14:55:37 +0100

    Fix - Update manifest to load sw

--- app/controllers/service_worker_controller.rb
+++ app/controllers/service_worker_controller.rb
@@ -1,28 +1,6 @@
 class ServiceWorkerController < ApplicationController
   # If you are using devise with user, skip authenticate
   # skip_before_action :authenticate_user!
-  # without skipping CSRF ActionController::InvalidCrossOriginRequest
-  # (Security warning: an embedded <script> tag on another site requested protected JavaScript.
-  # If you know what you're doing, go ahead and disable forgery protection
-  # on this action to permit cross-origin JavaScript embedding.):
-
-  protect_from_forgery except: :service_worker
-
   def service_worker
-    # different tests to try to solve MIME issue
-    # render partial: '/service_worker', status: 200
-    # respond_to do |format|
-    #   format.js { render content_type: 'text/javascript', status: 200 }
-    # end
-    # respond_to do |format|
-    #   format.js { render file: File.read(Rails.root.join('app', 'views', 'service-worker.js'), content_type: 'application/javascript') }
-    # end
-    # respond_to do |format|
-    #   format.js { render layout: false }
-    # end
-    # respond_to do |format|
-    #   format.js { }
-    # end
-    # render 'service-worker', layout: false
   end
 end
--- app/javascript/custom/companion.js
+++ app/javascript/custom/companion.js
@@ -1,12 +1,14 @@
-if (navigator.serviceWorker) {
-  console.log("in the companion")
-  navigator.serviceWorker.register("../service-worker.js", { scope: "/" })
-    .then(() => navigator.serviceWorker.ready)
-    .then((registration) => {
-      if ("SyncManager" in window) {
-        registration.sync.register("sync-forms");
-      } else {
-        window.alert("This browser does not support background sync.")
-      }
-    }).then(() => console.log("[Companion]", "Service worker registered!"));
-}
+window.addEventListener('load', () => {
+  // Is service worker available?
+  if (navigator.serviceWorker) {
+    navigator.serviceWorker.register("/service-worker", { scope: "/" })
+      .then(() => navigator.serviceWorker.ready)
+      .then((registration) => {
+        if ("SyncManager" in window) {
+          registration.sync.register("sync-forms");
+        } else {
+          window.alert("This browser does not support background sync.")
+        }
+      }).then(() => console.log("[Companion]", "Service worker registered!"));
+  }
+});
