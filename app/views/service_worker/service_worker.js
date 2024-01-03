c9b35386c7021e619e1475400953620cc13e04fb
Francois DUMAS LATTAQUE <dumaslattaque.francois@gmail.com>
2024-01-03 17:42:41 +0100

    Feat - Complete caching of assets

--- app/views/service_worker/service_worker.js
+++ app/views/service_worker/service_worker.js
@@ -19,11 +19,36 @@ importScripts(
   "https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js"
 );
 
-const { CacheFirst } = workbox.strategies;
+// We first define the strategies we will use and the registerRoute function
+
+const { CacheFirst, NetworkFirst } = workbox.strategies;
+const { registerRoute } = workbox.routing;
+
+// For every other page we use network first to ensure the most up-to-date resources
+registerRoute(
+  ({ request, url }) =>
+    request.destination === "document" || request.destination === "",
+  new NetworkFirst({
+    cacheName: "documents",
+  })
+);
+
+// For assets (scripts and images), we use cache first
+registerRoute(
+  ({ request }) =>
+    request.destination === "script" || request.destination === "style",
+  new CacheFirst({ cacheName: "assets-styles-and-scripts" })
+);
+registerRoute(
+  ({ request }) => request.destination === "image",
+  new CacheFirst({ cacheName: "assets-images" })
+);
+
+
 const { warmStrategyCache } = workbox.recipes;
 const { setCatchHandler } = workbox.routing;
 
-// Define what should be cached
+// Define which page should be cached
 const strategy = new CacheFirst();
 const urls = [
   '/offline.html',
