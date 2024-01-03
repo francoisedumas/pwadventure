60725d13c41760d5fa34a1a479a6c70910648a65
Francois DUMAS LATTAQUE <dumaslattaque.francois@gmail.com>
2024-01-03 15:08:25 +0100

    Feat - Move offline to service worker controller

--- app/controllers/service_worker_controller.rb
+++ app/controllers/service_worker_controller.rb
@@ -1,6 +1,11 @@
 class ServiceWorkerController < ApplicationController
   # If you are using devise with user, skip authenticate
   # skip_before_action :authenticate_user!
+
   def service_worker
+  end
+
+  def offline
+    render "offline", layout: false
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :quotes
 
-  get "offline", to: "pages#offline"
+  get "offline", to: "service_worker#offline"
   get "service-worker.js", to: "service_worker#service_worker"
 
   root "quotes#index"
