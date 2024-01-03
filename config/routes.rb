5764ce7177b5898daa0c5cbd2e06bf8a05cee7ea
Francois DUMAS LATTAQUE <dumaslattaque.francois@gmail.com>
2024-01-03 10:20:00 +0100

    Feat - Service worker setup

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :quotes
 
   get "offline", to: "pages#offline"
+  get "service-worker.js", to: "service_worker#service_worker"
 
   root "quotes#index"
 end
