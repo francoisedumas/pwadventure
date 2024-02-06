709c0a20e46d3665db8dafe07836647038465ff4
Francois DUMAS LATTAQUE <dumaslattaque.francois@gmail.com>
2024-02-06 13:51:17 +0100

    Feat - Ensure we don't create new subscription if already existing

--- app/controllers/webpush_subscriptions_controller.rb
+++ app/controllers/webpush_subscriptions_controller.rb
@@ -2,11 +2,15 @@ class WebpushSubscriptionsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    subscription = WebpushSubscription.new(
-      endpoint: params[:endpoint],
-      auth_key: params[:keys][:auth],
-      p256dh_key: params[:keys][:p256dh]
-    )
+    subscription = WebpushSubscription.find_by(auth_key: params[:keys][:auth])
+
+    if !subscription
+      subscription = WebpushSubscription.new(
+        endpoint: params[:endpoint],
+        auth_key: params[:keys][:auth],
+        p256dh_key: params[:keys][:p256dh]
+      )
+    end
 
     if subscription.save
       render json: subscription
