eb1aa3ce55bd68bba60f2dbedd4c7da29f4ba597
Francois DUMAS LATTAQUE <dumaslattaque.francois@gmail.com>
2024-02-06 10:51:40 +0100

    Feat - Setup model

---
+++ app/models/webpush_subscription.rb
@@ -0,0 +1,2 @@
+class WebpushSubscription < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_02_100650) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_06_095002) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "quotes", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "webpush_subscriptions", force: :cascade do |t|
+    t.string "endpoint"
+    t.string "auth_key"
+    t.string "p256dh_key"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
